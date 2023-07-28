1052e8920cc621c4bf0a561c96556e008aa8e52c
Raghunandan <16raghunandangupta@gmail.com>
2023-07-28 20:45:34 +0530

    CF edu 152 -C

---
+++ cf/edu152C.cpp
@@ -0,0 +1,207 @@
+/*
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⣋⣯⡀⠀⣠⣿⠿⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣤⡙⣿⡄⠀⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⣠⡾⠙⠟⣩⡤⢾⠉⡿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣼⣿⡷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢀⣾⠿⠶⠶⠚⠉⡀⢰⣤⢳⠧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⢸⠃⠀⣿⡄⠀⠀⣷⡘⡿⠟⠃⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠠⠀⢸⣀⡀⠟⠉⣟⣛⣿⡴⠶⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⣩⣥⡴⠞⠋⠉⠀⠀⣀⣠⣤⣤⣶⣶⣶⣶⣶⠶⠶⢶⣶⣶⣦⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠶⠛⠉⠀⠀⠀⠀⣶⣶⣿⣿⣿⡿⠿⡿⣿⣿⣀⡀⠿⣦⡀⠀⠈⠉⠙⠛⠛⠿⣿⣯⣰⢤⣄⡀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⣀⣴⣶⠿⠋⠉⠁⠀⠀⠀⠀⠀⠈⠻⣿⣷⡀⠙⠿⣦⡀⠀⠀⠀⠀⠀⠀⠉⠙⠻⢶⣧⣹⣦⣀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⢠⣴⠿⡛⣁⡀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠈⠻⣷⡀⠀⠈⠳⡄⠀⠀⠀⠀⢲⡀⠀⠀⠀⠈⠉⠀⠙⣧⡀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⢠⣾⣶⡿⣧⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⢀⣬⣿⡀⠀⠀⠘⢦⡀⠀⢀⣄⠻⣄⠀⠀⠀⠀⠀⠀⠸⠿⣦⡀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠀⢴⣿⣿⣿⣿⠟⠛⠛⠛⠛⣿⣛⣛⠛⠛⠋⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣦⣀⡀⠈⠻⣷⣌⢻⣷⣭⣑⠀⠀⠀⠸⣦⡀⠀⠸⣧⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⡟⠰⣿⣿⣿⠋⣡⣴⣾⣿⣷⣶⣶⣯⣙⠿⣷⣦⡀⠀⠀⠀⠀⣼⣿⣿⣿⣉⣬⣿⣿⣶⣾⣿⣿⣷⣝⢿⣿⣿⣶⣀⣄⠙⢿⣦⡀⠀⡆⢸⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⢰⣿⣿⣿⠟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣻⡏⠀⠀⠀⠀⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣜⠿⢿⣿⣿⣦⣄⠙⣇⣤⣀⣸⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⠿⣿⣿⣟⣻⣿⣿⣿⣿⣟⣿⣿⣶⣀⣤⣄⣀⣰⣿⠟⠹⠿⢯⡿⣿⣿⣿⡿⠿⠿⢿⣿⣿⣷⣌⣻⣿⣿⣿⣿⣿⣿⢉⣛⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⢻⣿⡿⠛⠛⢿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡈⠙⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠋⢻⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠇⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠟⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠘⢻⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠀⢸⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡿⠀⠀⠀⠀⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠠⣾⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⢸⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⢁⣤⣤⣤⣄⡈⢿⣷⡀⠀⠀⠀⠀⠀⠀⠀⢀⢀⣴⣿⠏⠀⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣰⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⣿⣿⣿⣿⣶⣄⣀⣀⣀⣀⣤⣶⡿⠟⠹⣿⣻⣿⣿⣻⣿⡿⠻⢿⣦⣄⣀⡀⢀⣀⣠⣼⣿⡿⠃⠀⠀⣿⣿⣿⣿⣿⣿⡟⢿⣧⣰⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⣿⣿⡆⠉⠛⠛⠛⠛⠛⠛⠉⠀⠀⠀⠉⠉⠛⡿⠉⠁⠀⠀⠀⠉⠛⠛⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣇⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡶⠶⠶⠒⠒⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⡋⠉⠉⠙⠷⢿⣯⡙
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣥⣤⣤⣶⣶⣶⣦⣽⣷⣶⣤⣴⠆⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⢸⣿⡟⠀⠀⠀⠀⠀⢸⣿⠁
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠻⠾⠿⠿⠷⠶⢾⣛⠉⠁⠀⢀⣤⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⢸⣿⠁⠀⠀⠀⠀⠀⣾⠃⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⢿⡿⠀⠀⠀⠀⢀⣾⠃⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣟⣾⠃⠀⠀⠀⢠⡿⠁⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⢨⡟⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣯⠀⠀⠀⠀⢠⡿⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠾⠛⠁⠘⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⢀⡞⢀⠄⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠙⣃⠉⠛⠻⠶⣾⣿⣿⣿⣿⣶⣷⡶⠖⠒⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⡼⠁⡼⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢠⣿⣷⣤⠀⠄⠈⠉⠿⣿⣿⣿⠿⠄⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⣼⠃⡼⠁⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⢰⠇⡼⠁⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⢋⣴⣿⣭⣭⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠈⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣴⣿⣿⣿⣿⡟⠀⠀⠀⠀⢰⡟⠸⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⠟⣡⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠙⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡴⢆⣠⣾⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⣸⡇
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, a, n) for (i = a; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+/////////////////////////////////////////////////////////////////////////////////////////
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+////////////////////////////////////////CONST////////////////////////////////////////////
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+
+///////////////////////////////////////FUNCTION//////////////////////////////////////////
+
+int mpow(int base, int exp)
+{
+    base %= mod;
+    int result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+ll mod_mul(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+
+///////////////////////////////////////////////CODE////////////////////////////////////////////////////////////////////////
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+    vector<int> pre(n);
+    pre[0] = (s[0] == '1');
+    for (int i = 1; i < n; i++)
+    {
+        pre[i] = pre[i - 1] + (s[i] == '1');
+        /* debug(pre[i]); */
+    }
+    vector<int> one, zero;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+            one.pb(i);
+        else
+            zero.pb(i);
+    }
+    set<pii> st;
+    while (m--)
+    {
+        int l, r;
+        cin >> l >> r;
+        --l, --r;
+        /*  debug(l, r);
+         debug(pre[r], pre[l]); */
+        int count = pre[r];
+        if (l > 0)
+            count -= pre[l - 1];
+        if (l == r || count == 0 || count == r - l + 1)
+        {
+            st.insert({-1, -1});
+            /* debug(count); */
+            continue;
+        }
+        int lzero = upper_bound(all(zero), r) - zero.begin() - 1;
+        lzero = zero[lzero];
+        int fone = lower_bound(all(one), l) - one.begin();
+        fone = one[fone];
+        if (lzero > fone)
+            st.insert({lzero, fone});
+        else
+            st.insert({-1, -1});
+    }
+    cout << st.size() << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
