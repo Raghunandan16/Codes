3532dbd1623d9456d973bf31990dd0006cd79707
Raghunandan <16raghunandangupta@gmail.com>
2023-07-29 23:58:01 +0530

    div 2 889 CF

---
+++ cf/889C.cpp
@@ -0,0 +1,249 @@
+/*
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⣋⣯⡀⠀⣠⣿⠿⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣤⡙⣿⡄⠀⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⣠⡾⠙⠟⣩⡤⢾⠉⡿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣼⣿⡷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⢀⣾⠿⠶⠶⠚⠉⡀⢰⣤⢳⠧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⢸⠃⠀⣿⡄⠀⠀⣷⡘⡿⠟⠃⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠠⠀⢸⣀⡀⠟⠉⣟⣛⣿⡴⠶⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⣩⣥⡴⠞⠋⠉⠀⠀⣀⣠⣤⣤⣶⣶⣶⣶⣶⠶⠶⢶⣶⣶⣦⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠶⠛⠉⠀⠀⠀⠀⣶⣶⣿⣿⣿⡿⠿⡿⣿⣿⣀⡀⠿⣦⡀⠀⠈⠉⠙⠛⠛⠿⣿⣯⣰⢤⣄⡀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⣀⣴⣶⠿⠋⠉⠁⠀⠀⠀⠀⠀⠈⠻⣿⣷⡀⠙⠿⣦⡀⠀⠀⠀⠀⠀⠀⠉⠙⠻⢶⣧⣹⣦⣀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⢠⣴⠿⡛⣁⡀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠈⠻⣷⡀⠀⠈⠳⡄⠀⠀⠀⠀⢲⡀⠀⠀⠀⠈⠉⠀⠙⣧⡀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⢠⣾⣶⡿⣧⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⢀⣬⣿⡀⠀⠀⠘⢦⡀⠀⢀⣄⠻⣄⠀⠀⠀⠀⠀⠀⠸⠿⣦⡀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠀⢴⣿⣿⣿⣿⠟⠛⠛⠛⠛⣿⣛⣛⠛⠛⠋⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣦⣀⡀⠈⠻⣷⣌⢻⣷⣭⣑⠀⠀⠀⠸⣦⡀⠀⠸⣧⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⡟⠰⣿⣿⣿⠋⣡⣴⣾⣿⣷⣶⣶⣯⣙⠿⣷⣦⡀⠀⠀⠀⠀⣼⣿⣿⣿⣉⣬⣿⣿⣶⣾⣿⣿⣷⣝⢿⣿⣿⣶⣀⣄⠙⢿⣦⡀⠀⡆⢸⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⢰⣿⣿⣿⠟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣻⡏⠀⠀⠀⠀⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣜⠿⢿⣿⣿⣦⣄⠙⣇⣤⣀⣸⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⠿⣿⣿⣟⣻⣿⣿⣿⣿⣟⣿⣿⣶⣀⣤⣄⣀⣰⣿⠟⠹⠿⢯⡿⣿⣿⣿⡿⠿⠿⢿⣿⣿⣷⣌⣻⣿⣿⣿⣿⣿⣿⢉⣛⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⢻⣿⡿⠛⠛⢿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡈⠙⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠋⢻⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠇⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠟⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠘⢻⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠀⢸⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡿⠀⠀⠀⠀⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠠⣾⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⢸⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⢁⣤⣤⣤⣄⡈⢿⣷⡀⠀⠀⠀⠀⠀⠀⠀⢀⢀⣴⣿⠏⠀⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣰⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠈⣿⣿⣿⣿⣶⣄⣀⣀⣀⣀⣤⣶⡿⠟⠹⣿⣻⣿⣿⣻⣿⡿⠻⢿⣦⣄⣀⡀⢀⣀⣠⣼⣿⡿⠃⠀⠀⣿⣿⣿⣿⣿⣿⡟⢿⣧⣰⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣿⣿⣿⡆⠉⠛⠛⠛⠛⠛⠛⠉⠀⠀⠀⠉⠉⠛⡿⠉⠁⠀⠀⠀⠉⠛⠛⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣇⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡶⠶⠶⠒⠒⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⡋⠉⠉⠙⠷⢿⣯⡙
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣥⣤⣤⣶⣶⣶⣦⣽⣷⣶⣤⣴⠆⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⢸⣿⡟⠀⠀⠀⠀⠀⢸⣿⠁
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠻⠾⠿⠿⠷⠶⢾⣛⠉⠁⠀⢀⣤⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⢸⣿⠁⠀⠀⠀⠀⠀⣾⠃⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⢿⡿⠀⠀⠀⠀⢀⣾⠃⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣟⣾⠃⠀⠀⠀⢠⡿⠁⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⢨⡟⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣯⠀⠀⠀⠀⢠⡿⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠾⠛⠁⠘⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⢀⡞⢀⠄⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠙⣃⠉⠛⠻⠶⣾⣿⣿⣿⣿⣶⣷⡶⠖⠒⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⡼⠁⡼⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢠⣿⣷⣤⠀⠄⠈⠉⠿⣿⣿⣿⠿⠄⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⣼⠃⡼⠁⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⢰⠇⡼⠁⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⢋⣴⣿⣭⣭⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠈⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣴⣿⣿⣿⣿⡟⠀⠀⠀⠀⢰⡟⠸⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⣿⣿⣿⣿⠟⣡⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠙⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡴⢆⣠⣾⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⣸⡇
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, a, n) for (i = a; i < n; i++)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+/////////////////////////////////////////////////////////////////////////////////////////
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+
+////////////////////////////////////////CONST////////////////////////////////////////////
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+
+///////////////////////////////////////FUnegION//////////////////////////////////////////
+
+int mpow(int base, int exp)
+{
+    base %= mod;
+    int result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+ll mod_mul(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+
+///////////////////////////////////////////////CODE////////////////////////////////////////////////////////////////////////
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int maxi = 0, mini = 0;
+    int maxInd, minInd;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] > maxi)
+        {
+            maxInd = i + 1;
+            maxi = a[i];
+        }
+        if (a[i] < mini)
+        {
+            minInd = i + 1;
+            mini = a[i];
+        }
+    }
+    // base check
+    if (n == 1)
+    {
+        cout << "0\n";
+        return;
+    }
+    if (maxi == mini)
+    {
+        cout << "0\n";
+        return;
+    }
+    if (mini == 0)
+    {
+        cout << n - 1 << "\n";
+        for (int i = 0; i < n - 1; i++)
+        {
+            cout << i + 2 << " " << i + 1 << "\n";
+        }
+        return;
+    }
+    if (maxi == 0)
+    {
+        cout << n - 1 << "\n";
+        for (int i = n - 2; i >= 0; i--)
+        {
+            cout << i + 1 << " " << i + 2 << "\n";
+        }
+        return;
+    }
+    //
+    vector<pair<int, int>> v;
+    if (maxi >= abs(mini))
+    {
+        int l = maxInd;
+        for (int i = 0; i < n; i++)
+        {
+            if (maxi != a[i] && a[i] < 0)
+            {
+                v.push_back({i + 1, l});
+                a[i] += maxi;
+            }
+        }
+        for (int i = 0; i < n - 1; i++)
+        {
+            v.push_back({i + 2, i + 1});
+        }
+    }
+    else
+    {
+        int l = minInd;
+        for (int i = 0; i < n; i++)
+        {
+            if (mini != a[i] && a[i] > 0)
+            {
+                v.push_back({i + 1, l});
+                a[i] += mini;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--)
+        {
+            v.push_back({i + 1, i + 2});
+        }
+    }
+    cout << v.size() << "\n";
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i].first << " " << v[i].second << "\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
